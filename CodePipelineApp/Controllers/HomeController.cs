9696a062eee8746430d9e7fd04d90300d3ffd251
ashutoshpandeydu <ashutosh.pandey@dataunveil.com>
2022-01-15 18:57:38 +0530

    First commit

---
+++ CodePipelineApp/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodePipelineApp.Controllers
+{
+    [ApiController]
+    [Route("/home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("status")]
+        public IActionResult Status()
+        {
+            return Ok(new { Status = "Working fine" });
+        }
+    }
+}
